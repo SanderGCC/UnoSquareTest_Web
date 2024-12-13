48040de9cfbb0f62560da433e670a6fd7dae88ee
SanderGCC <sandergcc@gmail.com>
2024-12-13 12:13:56 -0500

    Avances container tasks

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { TaskCardComponent } from "../../components/task-card/task-card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [TaskCardComponent],
+  imports: [
+    TaskCardComponent,
+    MatIconModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
