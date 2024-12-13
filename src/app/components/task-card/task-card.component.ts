b9ac4ad5188bee861d92e6deebc9e5179cc1f77f
SanderGCC <sandergcc@gmail.com>
2024-12-13 11:31:55 -0500

    Avances Diseño card

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,8 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-task-card',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.scss'
 })
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TaskCardComponent } from "../../components/task-card/task-card.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [TaskCardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
